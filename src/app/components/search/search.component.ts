5fb5d88f0a1fca8c1d764850982f198a858f16b2
Vishnu Vijayan <vvijayan@purdue.edu>
2017-05-24 12:26:07 +0530

    Display artist info from observable

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../../../../Artist';
 
 @Component({
   selector: 'search',
@@ -9,6 +10,7 @@ import { SpotifyService } from '../../services/spotify.service';
 
 export class SearchComponent  {
   searchStr: string;
+  searchRes: Artist[];
 
   constructor( private _spotifyService: SpotifyService) {
 
@@ -16,7 +18,7 @@ export class SearchComponent  {
 
   searchMusic() {
     this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
-      console.log(res);
+      this.searchRes = res.artists.items;
     });
   }
 }
